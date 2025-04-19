cc36e0fc12474b88e79a3b14ed9f891b03f188db
YasmeenTarek1 <yasmeen.tarek.elabsawy@gmail.com>
2025-04-19 20:31:35 +0200

    Aligning Scheduler logic with newly created PCB and Process structs & Enhanced printing

--- Milestone_2/src/MLFQ.c
+++ Milestone_2/src/MLFQ.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "queue.h"
-#include "PCB.h"
+#include "Queue.h"
+#include "process.h"
 
 void runMLFQ(Process* processes[], int numProcesses) {
     printf("MLFQ Scheduler Simulation\n");
@@ -11,21 +11,22 @@ void runMLFQ(Process* processes[], int numProcesses) {
         queues[i] = createQueue();
     
     int finished = 0;   // Number of finished processes
-    int time = 0;       // Current Cycle of the CPU
+    int time = 0;       // Current Cycle of the CPU (incremented at the end of each cycle)
+
 
     while(finished < numProcesses){
+        printf("\nTime %d: \n \n", time);
 
         // Add newly arrived processes to highest priority queue
-        for (int i = 0; i < numProcesses; i++) { // what happens if 2/more arrived at the same time ?????????
-            if (processes[i]->arrivalTime == time) {
-                processes[i]->priority = 0;
-                processes[i]->state = READY;
+        for (int i = 0; i < numProcesses; i++) {        // what happens if 2/more arrived at the same time ?????????
+            if (processes[i]->ready_time == time) {
+                // current->priority = 0;
                 enqueue(queues[0], processes[i]);
-                printf("Time %d: Process %d arrived\n", time, processes[i]->id);
+                printf("Process %d arrived\n", processes[i]->pid);
             }
         }
 
-        // Display the queues
+        // Display the ready queues
         display(queues[0]);
         display(queues[1]);
         display(queues[2]);
@@ -49,33 +50,39 @@ void runMLFQ(Process* processes[], int numProcesses) {
 
             // Execute Process for the time quantum
             for (int j = 0; j < timeQuantum && current->remainingTime > 0; j++) {
-                if(j == 0)
-                    printf("Time %d: Running process %d in queue %d with time quantum %d\n", time+1, current->id, level+1, timeQuantum);
 
-                current->programCounter++;
-                // execute the instruction that has this program counter
+                if(j == 0)
+                    printf("Running process %d in queue %d with time quantum %d\n", current->pid, level+1, timeQuantum);
+
+                // Execute(current->pid); // Simulate the execution of the process
                 current->remainingTime--;
-                time++; // Update the current cycle of the CPU
-                
-                printf("Time %d: Executing instruction %d of process %d\n", time, current->programCounter, current->id);
+                printf("Executing next instruction of process %d\n", current->pid);
+
+                if(current->remainingTime > 0 && j+1 < timeQuantum){ // Not the final cycle
+                    time++; 
+                    printf("\nTime %d:\n \n", time);
+                }
             }
             
             if (current->remainingTime == 0) {
-                printf("Process %d finished\n", current->id);
+                printf("Process %d finished\n", current->pid);
                 finished++;
+                current->state = TERMINATED;
             } else {
                 if(level != 3) {
-                    current->priority = level+1;
+                    // current->priority = level+1;
                     enqueue(queues[level+1], current); // Move to next queue
-                    printf("Time %d: Time slice ended, demoting process %d to level %d\n", time, current->id, level+2);
+                    printf("Process %d is moved to level %d\n", current->pid, level+2);
                 }
                 else{
                     enqueue(queues[level], current); // Stay in the same queue (Last Queue - RR)
-                    printf("Time %d: Time slice ended, demoting process %d to level %d\n", time, current->id, level+1);
+                    printf("Process %d is moved to level %d\n", current->pid, level+1);
                 }
+                current->state = READY;
             }
+            time++;
         }else{
-            printf("Time %d: CPU is idle\n", time++);
+            printf("CPU is idle\n", time++);
         }
 
     }
@@ -88,13 +95,13 @@ int main() {
     int n = 4;
     Process* processes[n];
 
-    int arrival[] = {0, 2, 2, 3};
+    int ready[] = {0, 2, 2, 3};
     int burst[] = {1, 3, 8, 15};
 
     for (int i = 0; i < n; i++) {
         processes[i] = (Process*)malloc(sizeof(Process));
-        processes[i]->id = i + 1;
-        processes[i]->arrivalTime = arrival[i];
+        processes[i]->pid = i + 1;
+        processes[i]->ready_time = ready[i];
         processes[i]->burstTime = burst[i];
         processes[i]->remainingTime = burst[i];
         processes[i]->next = NULL;
