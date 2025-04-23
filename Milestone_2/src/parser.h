2fb00386689e6850389b423066ff016a45bc84f0
Layla Khaled <laila.khaled.04@gmail.com>
2025-04-23 19:32:21 +0200

    final isa

--- Milestone_2/src/parser.h
+++ Milestone_2/src/parser.h
@@ -39,21 +39,17 @@ typedef struct
     InstructionType value; // Instruction type (e.g., PRINT)
 } DecodeHashEntry;
 
-// Execution hashmap entry (maps instruction type to function pointer)
-typedef struct
-{
-    InstructionType key;                   // Instruction type (e.g., PRINT)
-    void (*handler)(PCB *, Instruction *); // Function pointer to syntax function
-} ExecuteHashEntry;
-
 // Function prototypes
 // Fetching
-char *fetch_instruction(MemoryWord *memory, IndexEntry *index, PCB *pcb, Process *process);
+char* fetch_instruction(PCB* pcb, int pid);
 
 // Decoding
-Instruction decode_instruction(char *instruction_string);
+Instruction decode_instruction(char* instruction_string);
 
 // Execution - Main function
-void execute_instruction(MemoryWord *memory, PCB *pcb, Process *process, Instruction *instruction);
+void execute_instruction(PCB* pcb, int pid, Instruction* instruction);
+
+// Execution Cycle
+void exec_cycle(int pid);
 
 #endif // PARSER_H
