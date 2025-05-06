09c8feef2acad14421bfaf273224fd7ccdb07bc6
Mai Hazem <maihazem607@gmail.com>
2025-05-06 20:36:33 +0300

    Adding colors to console Loggings.

--- Milestone_2/src/console_model.c
+++ Milestone_2/src/console_model.c
@@ -24,18 +24,20 @@ typedef struct
 void console_model_init(void)
 {
     // Create logging directory if it doesn't exist
-    #ifdef _WIN32
-        if (!CreateDirectory("logging", NULL) && GetLastError() != ERROR_ALREADY_EXISTS) {
-            g_warning("Failed to create logging directory");
-            return;
-        }
-    #else
-        if (mkdir("logging", 0755) != 0 && errno != EEXIST) {
-            g_warning("Failed to create logging directory");
-            return;
-        }
-    #endif
-    
+#ifdef _WIN32
+    if (!CreateDirectory("logging", NULL) && GetLastError() != ERROR_ALREADY_EXISTS)
+    {
+        g_warning("Failed to create logging directory");
+        return;
+    }
+#else
+    if (mkdir("logging", 0755) != 0 && errno != EEXIST)
+    {
+        g_warning("Failed to create logging directory");
+        return;
+    }
+#endif
+
     // Open the log file
     log_file = fopen("logging/console_log.txt", "w");
     if (!log_file)
